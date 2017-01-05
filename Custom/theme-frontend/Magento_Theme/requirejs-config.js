ac72c7bb27bdbbed904e19fd067a289418524f64
yudhbir <sachinsingh3066@gmail.com>
2017-01-05 11:44:19 +0530

    Theme commit done

---
+++ Custom/theme-frontend/Magento_Theme/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            jquery_cookie:           'Magento_Theme/js/jquery.cookie',
+            owl_carousel:           'Magento_Theme/js/owl.carousel.min',
+        }
+    }
+};
